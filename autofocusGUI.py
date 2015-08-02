aedb16c7bc519bb4e168e5d39c43e54d352e60d9
Ivan <floyd14@gmail.com>
2015-08-02 15:54:25 +0500

    add first widgets in GUI

--- autofocusGUI.py
+++ autofocusGUI.py
@@ -1,6 +1,14 @@
 from tkinter import *
 
 root = Tk()
-theLabel = Label(root, text = 'tasks')
-theLabel.pack()
+
+label_add = Label(root, text = 'New task')
+entry_add = Entry(root)
+button_add = Button(root, text='Add')
+
+label_add.grid(row=0, column=0)
+entry_add.grid(row=0, column=1)
+button_add.grid(row=0, column=2)
+
+
 root.mainloop()
