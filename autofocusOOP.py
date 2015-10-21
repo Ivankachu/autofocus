82a4ee883748a70e44f0d693a183b583b0df019d
unknown <floyd14@gmail.com>
2015-10-21 11:00:31 +0500

    cont_later method

--- autofocusOOP.py
+++ autofocusOOP.py
@@ -16,14 +16,24 @@ class WritingPad:
     def do_task(self, numtask):
         current_page = active[0]
         self.pages[current_page][numtask].do_task()
+        if self.check_complete_page():
+            self.turn_the_page()
 
-    def contin_later(self, task):
-        pass
+    def contin_later(self, numtask):
+        current_page = active[0]
+        text = self.pages[current_page][numtask].text
+        self.do_task(numtask)
+        self.add_task(text)
+        if self.check_complete_page():
+            self.turn_the_page()
 
     def kill_page(self):
         pass
 
     def turn_the_page(self):
+        pass
+
+    def check_complete_page(self):
         pass
 
 
