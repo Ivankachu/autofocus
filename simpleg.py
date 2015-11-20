ddd41476409d0e4a3b9bfc89be6927f35e543871
unknown <floyd14@gmail.com>
2015-11-20 15:45:48 +0500

    make unable to use win1 when win2 is open

--- simpleg.py
+++ simpleg.py
@@ -9,6 +9,7 @@ class App:
         self.root.mainloop()
     def pushme(self):
         self.newwin = Toplevel(self.root)
+        self.newwin.grab_set()
         self.showli = self.li[:]
         self.lab = Label(self.newwin, text=self.showli.pop(0))
         self.lab.pack()
