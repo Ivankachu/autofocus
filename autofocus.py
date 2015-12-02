546c1493fe859a29fcdbff2b743d3ebaba066acd
unknown <floyd14@gmail.com>
2015-12-02 19:15:14 +0500

    add stiky in grid

--- autofocus.py
+++ autofocus.py
@@ -31,29 +31,32 @@ class Autofocus:
 
         
 class MainWin:
-    
+
     def __init__(self, master):
         self.master = master
         self.width = 500
         self.height = 500
-        self.master.geometry('{}x{}'.format(self.width, self.height))
+        #self.master.geometry('{}x{}'.format(self.width, self.height))
 
         self.lbox      = tk.Listbox(self.master, height=20, width=50,
                                     activestyle = 'none')
         self.inputbox  = tk.Entry(self.master)
-        self.btchoose  = tk.Button(self.master, text='Choose!')
-        self.btchooseq = tk.Button(self.master, text='Choose quick!')
-        self.btdone    = tk.Button(self.master, text='Done!')
-        self.btcont    = tk.Button(self.master, text='Continue\nlater')
-        self.btadd     = tk.Button(self.master, text='Add')
+        self.btchoose  = self.make_bt(text='Choose!')
+        self.btchooseq = self.make_bt(text='Choose quick!')
+        self.btdone    = self.make_bt(text='Done!')
+        self.btcont    = self.make_bt(text='Continue\nlater')
+        self.btadd     = self.make_bt(text='Add')
         
         self.lbox.grid     (rowspan=4, column=0)
-        self.inputbox.grid (row=4, column=0)
-        self.btchoose.grid (row=0, column=1)
-        self.btchooseq.grid(row=1, column=1)
-        self.btdone.grid   (row=2, column=1)
-        self.btcont.grid   (row=3, column=1)
-        self.btadd.grid    (row=4, column=1)
+        self.inputbox.grid (row=4, column=0, sticky=W+E+N+S)
+        self.btchoose.grid (row=0, column=1, sticky=W+E+N+S, padx=5, pady=5)
+        self.btchooseq.grid(row=1, column=1, sticky=W+E+N+S, padx=5, pady=5)
+        self.btdone.grid   (row=2, column=1, sticky=W+E+N+S, padx=5, pady=5)
+        self.btcont.grid   (row=3, column=1, sticky=W+E+N+S, padx=5, pady=5)
+        self.btadd.grid    (row=4, column=1, sticky=W+E+N+S, padx=5, pady=5)
+
+    def make_bt(self, text=""):
+        return tk.Button(self.master, text=text)
 
 
 class ChooseWin:
