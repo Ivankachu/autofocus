b8917f14ca18d9af26ff5375d8e316a6458f7358
unknown <floyd14@gmail.com>
2015-12-21 09:18:29 +0500

    trying to do with 3 classes

--- tmp2.py
+++ tmp2.py
@@ -1,22 +1,28 @@
 import tkinter as tk
 
-class Demo1:
+class App:
     def __init__(self, master):
+        self.win1 = Demo1(master, self)
+    def fun_create(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.newWindow.grab_set()
+        self.app = Demo2(self.newWindow, self)
+    def fun_close(self):
+        pass
+
+
+class Demo1:
+    def __init__(self, master, parent):
         self.data = ''
         self.master = master
         self.frame = tk.Frame(self.master)
         self.lab = tk.Label(self.frame, text = self.data)
         self.button1 = tk.Button(self.frame, text = 'New Window',
-                                 width = 25, command = self.new_window)
+                                 width = 25, command = parent.fun_create)
         self.lab.pack()
         self.button1.pack()
         self.frame.pack()
 
-    def new_window(self):
-        self.newWindow = tk.Toplevel(self.master)
-        self.newWindow.grab_set()
-        self.app = Demo2(self.newWindow, self)
-        
     def get_data(self):
         self.data = self.app.data
         self.lab.configure(text = self.data)
@@ -43,7 +49,7 @@ class Demo2:
 
 def main(): 
     root = tk.Tk()
-    app = Demo1(root)
+    app = App(root)
     root.mainloop()
 
 if __name__ == '__main__':
