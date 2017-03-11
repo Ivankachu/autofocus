25dd2097a83c99fb00a1aaef380d6af9e49e182f
chupika <floyd14@gmail.com>
2017-03-11 22:18:14 +0500

    adjust setup.py to compile by cx_freeze

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ from cx_Freeze import setup, Executable
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
-    
+
+
+includes      = []
+include_files = [r"C:\Users\floyd\AppData\Local\Programs\Python\Python36\DLLs\tcl86t.dll", \
+                 r"C:\Users\floyd\AppData\Local\Programs\Python\Python36\DLLs\tk86t.dll"]
+
 setup(
     name = "Autofocus",
-    version = "0.2",
+    version = "0.3",
     description = "App of Autofocus System. The system is designed by Mark Forster",
+    options = {"build_exe": {"includes": includes, "include_files": include_files}},
     executables = [Executable("autofocus.py", base=base)]
 )
